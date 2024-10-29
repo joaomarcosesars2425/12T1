952902737317385aa07e56366d9810b1accfeb50
joaomarcosesars2425 <joaomarcos@esars.pt>
2024-10-29 09:02:41 +0000

    Primeiro commit - calculadora basica

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+# Função para soma
+def soma(x, y):
+    return x + y
+
+# Função principal
+def calculadora():
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
+    result = soma(num1, num2)
+    print("Soma: " + str(result))
+
+# Executa a calculadora
+calculadora()
